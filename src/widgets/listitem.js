dc303952a6530073edd4aad8f5ac7b0df7adc2bb
devac0301 <hdev0301@gmail.com>
2017-10-29 05:47:12 +0900

    add list item widget

---
+++ src/widgets/listitem.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import ReactNative, {StyleSheet} from 'react-native';
+
+const { View, TouchableHighlight, Text } = ReactNative;
+
+class ListItem extends Component {
+  render() {
+    return (
+      <TouchableHighlight onPress={this.props.onPress}>
+        <View style={styles.li}>
+          <Text style={styles.liText}>{this.props.item.title}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  li: {
+    backgroundColor: '#fff',
+    borderBottomColor: '#eee',
+    borderColor: 'transparent',
+    borderWidth: 1,
+    paddingLeft: 16,
+    paddingTop: 14,
+    paddingBottom: 16,
+  },
+  liText: {
+    color: '#333',
+    fontSize: 16,
+  },
+});
+
+module.exports = ListItem;
