e7086df003416b2d0c29d290ed16dd525f00b39a
devac0301 <hdev0301@gmail.com>
2017-10-29 05:46:44 +0900

    add firebase configuration

---
+++ src/lib/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // Initialize Firebase
+    firebaseConfig: {
+	    apiKey: "AIzaSyBvs6P55nmLqK5RzsXgUR41XC3lh1AJOgs",
+	    authDomain: "malefemale-4559a.firebaseapp.com",
+	    databaseURL: "https://malefemale-4559a.firebaseio.com",
+	    projectId: "malefemale-4559a",
+	    storageBucket: "malefemale-4559a.appspot.com",
+	    messagingSenderId: "786987413406"
+    },
+}
